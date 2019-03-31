0e4f022e5d5eb44e151fc373ea5c9916a81cd59b
anythingtechpro <anythingtechpro@gmail.com>
2019-03-31 19:30:51 -0400

    protocol: check to see if there is any extraneous data in the deserialization buffer

--- src/core/protocol.c
+++ src/core/protocol.c
@@ -281,6 +281,15 @@ int deserialize_message(packet_t *packet, void **message)
       return 1;
   }
 
+  // check to see if there is any extraneous data in the buffer
+  uint32_t remaining_size = buffer_get_size(buffer);
+  if (remaining_size > 0)
+  {
+    LOG_ERROR("Could not deserialize packet, packet has extraneous data of size: %u!", remaining_size);
+    buffer_free(buffer);
+    return 1;
+  }
+
   buffer_free(buffer);
   return 0;
 }
