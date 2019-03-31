bf4c10d186fa7a6e38cbedeb74342c67f3ec5c8c
anythingtechpro <anythingtechpro@gmail.com>
2019-03-31 18:34:04 -0400

    protocol: if any of the transactions in a valid incoming block that's added to the mempool has a transaction that's currently pending in the mempool, then the transaction will be removed from the mempool as it's considered to be "set in stone" after that point...

--- src/core/protocol.c
+++ src/core/protocol.c
@@ -892,6 +892,19 @@ int block_header_sync_complete(net_connection_t *net_connection, block_t *block)
     }
   }
 
+  // if any of this block's transactions are still in our memory pool,
+  // remove them since they have been "set in stone" within the block...
+  for (int i = 0; i < block->transaction_count; i++)
+  {
+    transaction_t *tx = block->transactions[i];
+    assert(tx != NULL);
+
+    if (is_tx_in_mempool(tx))
+    {
+      assert(remove_tx_from_mempool(tx) == 0);
+    }
+  }
+
   return 0;
 }
 
