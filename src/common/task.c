70a0b8367e66ad43679b09c302fb0523d8107aa0
Caleb Marshall <anythingtechpro@gmail.com>
2019-07-12 12:33:18 -0400

    crypto: removed cryptonote utilities

--- src/common/task.c
+++ src/common/task.c
@@ -23,6 +23,7 @@
 // You should have received a copy of the MIT License
 // along with Vulkan. If not, see <https://opensource.org/licenses/MIT>.
 
+#include <assert.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <time.h>
