8d08d9b1d25bbb6c7ad4b6190e42ce0dd4d62447
Caleb Marshall <caleb.marshall108@gmail.com>
2019-11-16 12:00:54 -0500

    tests/unittests: added proof of work validation checks

--- tests/unittests/crypto_tests.c
+++ tests/unittests/crypto_tests.c
@@ -38,6 +38,8 @@
 #include "crypto/cryptoutil.h"
 #include "crypto/sha256d.h"
 
+#include "core/pow.h"
+
 SUITE(crypto_suite);
 
 TEST sha256_hash_tests(void)
@@ -144,8 +146,22 @@ TEST bignum_compact_tests(void)
   PASS();
 }
 
+TEST pow_validation_tests(void)
+{
+  ASSERT(check_proof_of_work("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f", 0x1d00ffff));
+  ASSERT(check_proof_of_work("000000000003ba27aa200b1cecaad478d2b00432346c3f1f3986da1afd33e506", 0x1b04864c));
+  ASSERT(check_proof_of_work("000000000000034a7dedef4a161fa058a2d67a173a90155f3a2fe6fc132e0ebf", 0x1a05db8b));
+  ASSERT(check_proof_of_work("000000000000000082ccf8f1557c5d40b21edabb18d2d691cfbf87118bac7254", 0x1900896c));
+  ASSERT(check_proof_of_work("000000000000000004ec466ce4732fe6f1ed1cddc2ed4b328fff5224276e3f6f", 0x1806b99f));
+  ASSERT(check_proof_of_work("00000000000000000024fb37364cbf81fd49cc2d51c09c75c35433c3a1945d04", 0x18009645));
+  ASSERT(check_proof_of_work("00000000000000000007316856900e76b4f7a9139cfbfba89842c8d196cd5f91", 0x1715a35c));
+  free_pow_limit_bn();
+  PASS();
+}
+
 GREATEST_SUITE(crypto_suite)
 {
   RUN_TEST(sha256_hash_tests);
   RUN_TEST(bignum_compact_tests);
+  RUN_TEST(pow_validation_tests);
 }
